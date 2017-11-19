ff0c20c389f1da0bd0bddf8081586d2e629597a1
Bernard Notarianni <bernard.notarianni@gmail.com>
2017-11-19 14:08:58 +0100

    propose une deuxieme solution

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# kata-apb
+# Kata APB
 
 Quelques solutions pour le [Kata APB](http://notarianni.org/article/kata-apb/)
 
+
+# Solution 1
+
+[apb1.hs](apb1.hs)
+
+- Distribue les etudiants sur toutes les formations disponibles
+- Aucun choix ou résultats de notes formulés
+- Pas de limite aux nombre de places disponibles
+
+# Solution 2
+
+[apb2.hs](apb2.hs)
+
+- Les étudiants sont répartis exactement selon leurs choix
+- Aucune limite au nombre de places disponibles et pas de critère à l'entrée
 
---
+++ apb2.hs
@@ -0,0 +1,29 @@
+import Test.Hspec
+
+data Formation = Formation String deriving (Show, Eq)
+data Etudiant  = Etudiant  { nom :: String
+                           , choix :: Formation
+                           }
+  deriving (Show, Eq)
+
+
+apb :: [ Etudiant ] -> [ Formation ] -> [ (Etudiant, Formation) ]
+apb [] _ = []
+apb ( e : autres) formations =
+  (e, choix e) : apb autres formations
+
+
+main = hspec $ do
+  describe "apb" $ do
+
+    it "donne aux a chaque etudiante la formation qu'elle demande" $ do
+      apb [e1,e2,e3] [f1, f2] `shouldBe`
+        [(e1,f1),
+         (e2,f2),
+         (e3,f2)]
+      where
+        f1 = Formation "Informatique"
+        f2 = Formation "Histoire"
+        e1 = Etudiant {nom="Jeanne",   choix=f1}
+        e2 = Etudiant {nom="Mathilde", choix=f2}
+        e3 = Etudiant {nom="Sophie",   choix=f2}
